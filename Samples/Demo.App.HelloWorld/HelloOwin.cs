1a5db4b3f235d87efb1fa1acc006b02af2977957
Eddie Garmon <eddie_garmon@hotmail.com>
2013-10-05 23:40:49 -0400

    Nowin server wrapper and demo

---
+++ Samples/Demo.App.HelloWorld/HelloOwin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using Simple.Owin;
+using Simple.Owin.Extensions;
+using Simple.Owin.Support;
+
+namespace Demo.App.HelloWorld
+{
+    using AppFunc = Func< // original OWIN app delegate
+        IDictionary<string, object>, //OWIN environment
+        Task // return
+        >;
+    using AppFunc2 = Func< // alternate OWIN app delegate
+        IDictionary<string, object>, //OWIN environment
+        Func<Task>, // the next pipeline component 
+        Task // return
+        >;
+
+    public static class HelloOwin
+    {
+        public static AppFunc2 App_SayHello {
+            get {
+                return (environment, next) => {
+                           var stream = environment.GetValueOrDefault<Stream>(OwinKeys.Response.Body);
+                           stream.Write("<h1>Hello from OWIN!</h1>");
+                           var currentUser = environment.GetValueOrDefault<IUser>(OwinKeys.Server.User, new User("*Missing*"));
+                           stream.Write(string.Format("<h2>User: {0}</h2>", currentUser.Name));
+                           environment[OwinKeys.Response.StatusCode] = 200;
+                           return TaskHelper.Completed();
+                       };
+            }
+        }
+
+        public static AppFunc2 Middleware_DumpOwinContext {
+            get {
+                return (environment, next) => {
+                           next()
+                               .Wait();
+                           var context = new OwinContext(environment);
+                           context.Response.Output.Write("<br/>" + context.DumpEnvironmentAsHtmlTable());
+                           return TaskHelper.Completed();
+                       };
+            }
+        }
+
+        public static AppFunc2 Middleware_Identity {
+            get {
+                return (environment, next) => {
+                           var path = environment.GetValueOrDefault<string>(OwinKeys.Request.Path);
+                           IUser currentUser = null;
+                           switch (path) {
+                               case "/none":
+                                   break;
+                               case "/bad":
+                                   environment[OwinKeys.Response.StatusCode] = 403; //forbidden
+                                   return TaskHelper.Completed();
+                               case "/":
+                                   currentUser = new User();
+                                   break;
+                               default:
+                                   currentUser = new User(path.Substring(1));
+                                   break;
+                           }
+                           environment.SetValue(OwinKeys.Server.User, currentUser);
+                           return next();
+                       };
+            }
+        }
+
+        private class User : IUser
+        {
+            public User(string name = null) {
+                IsAuthenticated = name != null;
+                Name = name ?? "Anonymous";
+                Guid = Guid.NewGuid();
+            }
+
+            public Guid Guid { get; private set; }
+
+            public bool IsAuthenticated { get; private set; }
+
+            public string Name { get; private set; }
+        }
+    }
+}
---
+++ Source/Simple.Owin.Servers.Nowin/Full-4.5/NowinServerWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+using Nowin;
+
+using Simple.Owin.Hosting;
+
+namespace Simple.Owin.Servers.Nowin
+{
+    public class NowinServerWrapper : IOwinServer
+    {
+        private readonly ServerBuilder _builder = new ServerBuilder();
+
+        public NowinServerWrapper(IPAddress address = null, int? port = null, X509Certificate certificate = null) {
+            if (address != null) {
+                _builder.SetAddress(address);
+            }
+            if (port != null) {
+                _builder.SetPort(port.Value);
+            }
+            if (certificate != null) {
+                _builder.SetCertificate(certificate);
+            }
+        }
+
+        public void Configure(IDictionary<string, object> environment) {
+            _builder.SetOwinCapabilities(environment);
+        }
+
+        public void SetAppFunc(Func<IDictionary<string, object>, Task> appFunc) {
+            _builder.SetOwinApp(appFunc);
+        }
+
+        public IDisposable Start() {
+            return _builder.Start();
+        }
+    }
+}
