2f9d5f0777954fa8a54fb8f7339fae5cf60f7eba
Eddie Garmon <eddie_garmon@hotmail.com>
2013-10-25 10:28:09 -0400

    Add swappable path mapping support
    default support for asp.net hosting and local disk executable folder mapping

---
+++ Source/Simple.Owin/Full-4.0/PathMapping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Simple.Owin.Extensions;
+
+namespace Simple.Owin
+{
+    public static class PathMapping
+    {
+        private static Func<string, string> _map;
+
+        public static Func<string, string> Map {
+            get { return _map ?? (_map = BuildDefaultMapper()); }
+            set { _map = value; }
+        }
+
+        private static Func<string, string> BuildDefaultMapper() {
+            return GetSystemWebMapper() ?? GetCodeBaseRelativeMapper();
+        }
+
+        private static Func<string, string> GetCodeBaseRelativeMapper() {
+            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetCallingAssembly();
+            var localPath = new Uri(assembly.EscapedCodeBase).LocalPath;
+            var path = Path.GetDirectoryName(localPath);
+            if (path == null) {
+                return null;
+            }
+            return virtualPath => Path.Combine(path,
+                                               virtualPath.TrimStart('/')
+                                                          .Replace('/', Path.DirectorySeparatorChar));
+        }
+
+        private static Func<string, string> GetSystemWebMapper() {
+            var systemWeb = AppDomain.CurrentDomain.GetAssemblies()
+                                     .FirstOrDefault(assembly => assembly.FullName.StartsWith("System.Web,"));
+            if (systemWeb != null) {
+                var hostingEnvironment = systemWeb.GetType("System.Web.Hosting.HostingEnvironment");
+                if (hostingEnvironment != null) {
+                    return hostingEnvironment.GetDelegate<Func<string, string>>("MapPath", throwOnBindFailure: false);
+                }
+            }
+            return null;
+        }
+    }
+}
